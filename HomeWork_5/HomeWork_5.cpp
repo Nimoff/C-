028df10e7422365b3c258959ab1063d2a8977f70
Sergey Fomin <4363481@gmail.com>
2022-05-29 19:57:25 +0300

    HomeWork_5_1

---
+++ HomeWork_5/HomeWork_5.cpp
@@ -0,0 +1,19 @@
+﻿#include <iostream>
+using namespace std;
+
+//Задание 1
+void arr(double ptr[], int size)
+{
+    for(size_t i = 0; i < size; i++)
+    {
+        cout << ptr[i] << ' ';
+    }
+}
+
+int main()
+{
+    const int s = 5;
+    double a[s] = { 3.2, 5.3, 8.7, 9.5 };
+    arr(a, s);
+}
+
